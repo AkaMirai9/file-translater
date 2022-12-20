f886af2beda9f837aeeb742d438e97a26f1798d8
AkaMirai9 <105291924+AkaMirai9@users.noreply.github.com>
2022-12-20 21:21:19 +0100

    Création de l'entrée et vérification des inputs

---
+++ fileTranslator/errorHandler/isEntryValid.py
@@ -0,0 +1,3 @@
+def isEntryValid(arguments: list):
+    if arguments.__len__() != 2:
+        exit("Il doit y avoir exactement 1 argument")
---
+++ fileTranslator/fileTranslator.py
@@ -0,0 +1,11 @@
+import sys
+
+from errorHandler.isEntryValid import isEntryValid
+
+
+def main():
+    isEntryValid(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
